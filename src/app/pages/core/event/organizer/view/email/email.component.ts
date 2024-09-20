3a885b5f5d329fecfa405da8b235263e81c2a27b
jjrn0203 <jonathan08192016@gmail.com>
2024-09-20 00:22:20 -0500

    create view email

--- src/app/pages/core/event/organizer/view/email/email.component.ts
+++ src/app/pages/core/event/organizer/view/email/email.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-email',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class EmailComponent {
 
+  protected form!:FormGroup;
+  protected readonly formBuilder = inject(FormBuilder);
+
+  constructor(){
+    this.buildForm();
+  }
+
+  buildForm(){
+    return this.form = this.formBuilder.group({
+      title:[null, [Validators.required]],
+      message: [null, [Validators.required]],
+      hasBeenRead:[null, [Validators.required]],
+    })
+  };
+
+  get titleField(){
+    return this.form.controls['title'];
+  };
+
+  get messageField(){
+    return this.form.controls['message'];
+  };
+
+  get hasBeenReadField(){
+    return this.form.controls['hasBeenRead'];
+  };
 }

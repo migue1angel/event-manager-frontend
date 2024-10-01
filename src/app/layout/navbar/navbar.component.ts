8864bdaf506785596c57270799fe9de0c60bca6a
migue1angel <macf06200@gmail.com>
2024-09-30 19:32:42 -0500

    add routes

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -3,68 +3,70 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent implements OnInit {
   items: any[] = [];
   profileItems: any[] = [];
   ngOnInit() {
-      this.items = [
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        route: 'core/event/public/home',
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star',
+        route: 'core/event/public/home',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
           {
-              label: 'Home',
-              icon: 'pi pi-home'
+            label: 'Core',
+            icon: 'pi pi-bolt',
+            route: 'core/event/public/event-list',
           },
           {
-              label: 'Features',
-              icon: 'pi pi-star'
-          },
-          {
-              label: 'Projects',
-              icon: 'pi pi-search',
-              items: [
-                  {
-                      label: 'Core',
-                      icon: 'pi pi-bolt',
-                      shortcut: '⌘+S'
-                  },
-                  {
-                      label: 'Templates',
-                      icon: 'pi pi-palette',
-                      items: [
-                          {
-                              label: 'Apollo',
-                              icon: 'pi pi-palette',
-                              badge: '2'
-                          },
-                          {
-                              label: 'Ultima',
-                              icon: 'pi pi-palette',
-                              badge: '3'
-                          }
-                      ]
-                  }
-              ]
-          },
-          {
-              label: 'Contact',
-              icon: 'pi pi-envelope',
-              badge: '3'
-          }
-      ];
-      this.profileItems = [
-        {
-            label: 'Options',
+            label: 'Templates',
+            icon: 'pi pi-palette',
             items: [
-                {
-                    label: 'Refresh',
-                    icon: 'pi pi-refresh'
-                },
-                {
-                    label: 'Export',
-                    icon: 'pi pi-upload'
-                }
-            ]
-        }
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+                badge: '2',
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+                badge: '3',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+        badge: '3',
+      },
+    ];
+    this.profileItems = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Refresh',
+            icon: 'pi pi-refresh',
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-upload',
+          },
+        ],
+      },
     ];
   }
 }

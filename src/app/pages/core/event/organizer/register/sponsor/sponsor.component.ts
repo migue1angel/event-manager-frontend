be5d654760428a31fceae3937059307437a2fae1
migue1angel <macf06200@gmail.com>
2024-10-03 11:23:54 -0500

    fix bugs

--- src/app/pages/core/event/organizer/register/sponsor/sponsor.component.ts
+++ src/app/pages/core/event/organizer/register/sponsor/sponsor.component.ts
@@ -1,5 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MessageService, PrimeIcons } from 'primeng/api';
 import { SponsorEnum } from '../../../../../../shared/enums';
 
@@ -8,44 +14,92 @@ import { SponsorEnum } from '../../../../../../shared/enums';
   templateUrl: './sponsor.component.html',
   styleUrl: './sponsor.component.scss',
 })
-export class SponsorComponent {
+export class SponsorComponent implements OnInit {
   protected form!: FormGroup;
   protected sponsorForm!: FormGroup;
+  private newSponsorsIndex?: number;
+
   private messageService = inject(MessageService);
   private readonly formBuilder = inject(FormBuilder);
-  protected SponsorEnum = SponsorEnum
-  
+  protected SponsorEnum = SponsorEnum;
+  uploadedFiles: any[] = [];
+
 
   constructor() {
     this.buildForm();
     this.buildSponsorForm();
   }
+  ngOnInit(): void {}
 
   buildForm() {
     this.form = this.formBuilder.group({
-      sponsors: this.formBuilder.array([], [Validators.minLength(1)]),
+      sponsors: this.formBuilder.array([], [Validators.required,Validators.minLength(1)]),
     });
   }
 
   buildSponsorForm() {
     this.sponsorForm = this.formBuilder.group({
       name: [null, [Validators.required]],
-      photo: [null, [Validators.required]],
+      photo: [null,[Validators.required]],
     });
   }
 
-  addSponsor() {
-    this.sponsors.push(this.formBuilder.group(this.sponsorForm.value));
+  onUpload(event:any){
+    this.photoField.setValue(event)
+    console.log(this.photoField.value);
+    
   }
-
+  
+  addSponsor() {
+    this.sponsorForm.markAllAsTouched();
+    if (this.sponsorForm.valid) {
+      this.sponsors.push(this.formBuilder.group(this.sponsorForm.value));
+      this.sponsorForm.reset()
+    } 
+  }
+  
+  editSponsor(index: number){
+    this.sponsorForm.setValue(this.sponsors.controls[index].value)
+    this.sponsors.removeAt(index)
+  }
+  
+  removeSponsor(index:number){
+    this.sponsors.removeAt(index)
+  }
   onSubmit() {
     this.form.markAllAsTouched();
     if (this.form.valid) {
       console.log('data enviada');
+    }else{
+      this.messageService.add({
+        severity: 'error',
+        detail: 'Se debe agregar al menos un sponsor',
+        life: 3000,
+      });
     }
   }
 
-  get sponsors():FormArray {
+  validateSponsorsControls() {
+    this.sponsors.valueChanges.subscribe(() => {
+      if(this.sponsors.controls.length >= 1){
+        this.sponsors.controls[this.newSponsorsIndex!]
+        .get('name')!
+        .addValidators([Validators.required]);
+      this.sponsors.controls[this.newSponsorsIndex!]
+        .get('photo')!
+        .addValidators([Validators.required]);
+      }
+    });
+  }
+
+  get sponsors(): FormArray {
     return this.form.get('sponsors') as FormArray;
+  }
+
+  get nameField(): AbstractControl {
+    return this.sponsorForm.controls['name'];
+  }
+  get photoField(): AbstractControl {
+    return this.sponsorForm.controls['photo'];
   }
 }
--- src/app/shared/enums/message-service.enum.ts
+++ src/app/shared/enums/message-service.enum.ts
@@ -1,4 +1,4 @@
 export enum ValidateFormEnum {
   message = 'Revise que todos los campos se han llenado correctamente',
-  severity = 'danger',
+  severity = 'error',
 }
